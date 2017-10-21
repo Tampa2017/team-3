fb3e0fe94b0385b57665dcccda3e6bf1b24471c7
Alex Lev <alev@iastate.edu>
2017-10-20 21:01:14 -0500

    Background sound, play and pause integrated, and included sound file

--- views/index.js
+++ views/index.js
@@ -1,9 +1,31 @@
+
 // Get the modal
 var modal = document.getElementById('id01');
+var goodSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/goodbell.mp3");
+var badSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/bad.mp3");
+var winSound = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/win.mp3");
+var back = new Audio("backWave.WAV");
+var Bbool = false;
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+function soundB()
+{
+  if(Bbool == false)
+  {
+  back.play();
+ document.getElementById("playB").innerHTML = "Pause 🔇";
+Bbool = true;
+}
+else {
+  back.pause();
+  document.getElementById("playB").innerHTML = "Play 🔊";
+  Bbool = false;
+}
 }
